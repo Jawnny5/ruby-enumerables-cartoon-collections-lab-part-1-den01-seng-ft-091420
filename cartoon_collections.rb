782805a363447329f980e2dc2c30c9f36d1a5caa
masterful-principle-0059 <william.neal83@gmail.com>
2020-09-08 16:45:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,9 +8,8 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  dwarf_list = []
-  array.each_with_index do |index, char|
-    dwarf_list[index] = char
+  array.each_with_index do |char, index|
+    puts "#{index}." + " " + "#{char}"
   end
   # Use `each_with_index` to enumerate over the provided array
   #
