53d99885d78e1f94d606535ec1d5e8b65b05200b
masterful-principle-0059 <william.neal83@gmail.com>
2020-09-08 16:37:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 
 def list_dwarves(array)
   array.each_with_index do |char, index|
-    puts char
+    puts "1." + char
   end
   # Use `each_with_index` to enumerate over the provided array
   #
