33b021acf506d5deeae262cbecf2e9c8185dcaaf
masterful-principle-0059 <william.neal83@gmail.com>
2020-09-08 16:46:42 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,16 @@
 def greet_characters(array)
+  array.each do |char|
+    puts "Hello" + " " + char + "!"
+  end
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
 end
 
 def list_dwarves(array)
+  array.each_with_index do |char, index|
+    puts "#{index + 1}." + " " + "#{char}"
+  end
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
